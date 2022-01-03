c611d323958a86fd6b47edc6251f9db45681f5e5
MaksaLegend <61109606+VanyushkovMM@users.noreply.github.com>
2022-01-03 19:51:48 +0300

    update tests
    added console output during tests collection

--- others/run_tests.py
+++ others/run_tests.py
@@ -2,7 +2,17 @@ import subprocess
 import sys
 import os
 
-def printTextFile(file, param, i, result):
+def printTextToFile(f, file, param, i, result):
+	if i <= 1:
+		f.write("-"*29 + '\n')
+		f.write(" Name:      "+str(file) + '\n')
+		f.write(" Values:    "+str(param[1]) + '\n')
+		f.write(" Processes: "+str(param[0]) + '\n')
+		f.write("-"*29 + '\n')
+	f.write("*"*13 + ' ' + str(i) + ' ' + "*"*13 + '\n')
+	f.write(result)
+
+def printText(file, param, i, result):
 	if i <= 1:
 		print("-"*29)
 		print(" Name:     ", file)
@@ -19,13 +29,18 @@ if len(argv) > 1: count = int(argv[1])
 path = "test_arguments.txt"
 if not os.path.isfile(path):
 	path = "others/" + path
-
 f = open(path, "r")
 text = f.read().split('\n')
 f.close()
 
 files = ["mpi_programs", "thread_programs"]
+
+path = "results.txt"
+if not os.path.isfile(path):
+	path = "../" + path
+f = open(path, "w")
 for line in text:
+	if line == '': continue
 	for file in files:
 		for i in range(count):
 			param = line.split()
@@ -34,5 +49,6 @@ for line in text:
 			com += param
 			if len(param) == 1: param = ["4"] + param
 			result = subprocess.run(com, stdout=subprocess.PIPE, text=True)
-			printTextFile(file, param, i+1, result.stdout)
-
+			printText(file, param, i+1, result.stdout)
+			printTextToFile(f, file, param, i+1, result.stdout)
+f.close()
