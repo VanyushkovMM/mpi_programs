d04fd8388cdd62c6aa509eb40e2a5feb331cee55
MaksaLegend <61109606+VanyushkovMM@users.noreply.github.com>
2022-01-06 18:14:13 +0300

    Update build_programs.py

--- build_programs.py
+++ build_programs.py
@@ -1,17 +1,39 @@
 import os
-from sys import argv
+import sys
+
+if sys.platform == 'win32':
+    import ctypes
+    kernel32 = ctypes.windll.kernel32
+    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 
 files=["mpi_programs", "thread_programs"]
 
+def checkBuild(path):
+	path += '/build/main/'
+	if sys.platform == 'win32':
+		path += 'Debug/main.exe'
+	else:
+		path += 'main'
+	if os.path.isfile(path):
+		print('\033[35m{}\033[37m'.format('Successfully build!')) # PURPLE
+	else:
+		print('\033[31m{}\033[37m'.format('Not build!')) # RED
+	print()
+
 def build_file(file):
-	build="cd build && cmake .. && cmake --build ."
+	build="/build && cmake .. && cmake --build ."
 	cd="cd "
 	if not os.path.isdir(file+"/build"):
 		os.system(cd+file+" && mkdir build")
-	os.system(cd+file+" && "+build)
+	com = cd+file+build
+	print('\033[33m{}\033[37m'.format('>>> '+com)) # YELLOW
+	os.system(com)
+	checkBuild(file)
 
-if len(argv) == 2 and (argv[1] in ["1", "2"]):
-	build_file(files[int(argv[1])-1])
+if len(sys.argv) == 2 and (sys.argv[1] in ["1", "2"]):
+	build_file(files[int(sys.argv[1])-1])
+elif len(sys.argv) == 2 and (sys.argv[1] in files):
+	build_file(sys.argv[1])
 else:
 	for file in files:
 		build_file(file)
